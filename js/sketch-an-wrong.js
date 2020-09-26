f387e880cd0c7ede3ed7a9d12966b60f6ef62b6d
FrancyG8 <55449605+FrancyG8@users.noreply.github.com>
2020-09-26 13:11:20 +0200

    Animazioni finali e inizio About

--- js/sketch-an-wrong.js
+++ js/sketch-an-wrong.js
@@ -8,7 +8,7 @@ function setup() {
 
 function draw() {
   var risulFin = parseInt(check12, 10);
-  $( "#parag" ).text( "Rezultatai: " + risulFin +  "/12" );
+  $( "#parag" ).text( "Результат: " + risulFin +  "/12" );
 
   if(frameCount >= 21000){ //more or less after 3 minutes
     backHome();
